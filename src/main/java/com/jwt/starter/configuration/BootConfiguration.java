0d90bb5c3ec1970b32e887fe72823155da49fedb
zzqfsy <zzqfsy@126.com>
2017-09-26 18:02:51 +0800

    running!!!

--- src/main/java/com/jwt/starter/configuration/BootConfiguration.java
+++ src/main/java/com/jwt/starter/configuration/BootConfiguration.java
@@ -2,34 +2,28 @@ package com.jwt.starter.configuration;
 
 import com.jwt.starter.properties.RequestMatchersProperties;
 import com.jwt.starter.runner.BootRunner;
-import com.jwt.starter.security.exception.JwtAuthenticationEntryPoint;
 import com.jwt.starter.security.service.JwtUserRulesService;
-import com.jwt.starter.security.utils.JwtTokenUtil;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableConfigurationProperties(RequestMatchersProperties.class)
 @ConditionalOnClass({JwtUserRulesService.class})
+@ComponentScan( basePackages = "com.jwt.starter.security",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, value = RestController.class),
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Service.class)})
 public class BootConfiguration {
     @Bean
     public BootRunner getTestRunner(){
         return new BootRunner();
     }
-
-    @Bean
-    @ConditionalOnMissingBean(name = "jwtAuthenticationEntryPoint")
-    public JwtAuthenticationEntryPoint getJwtAuthenticationEntryPoint(){
-        return new JwtAuthenticationEntryPoint();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(name = "jwtTokenUtil")
-    public JwtTokenUtil jwtTokenUtil(){
-        return new JwtTokenUtil();
-    }
-
 }
